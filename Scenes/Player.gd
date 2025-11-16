643f3cb423cebee1673fb9985e2a531f176e1a8f
kuyoul <kuyoul0123@gmail.com>
2025-11-16 12:44:13 +0900

    carving 토글 형식으로 구현, 카빙 로직 변경

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -6,10 +6,10 @@ export (int) var drag_friction = 500
 export (int) var scroll_speed = 200
 
 export (float) var carve_threshold = 150.0
-var previous_y_velocity = 0.0
+
 
 var velocity = Vector2.ZERO
-
+var is_carving = false
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -21,28 +21,36 @@ func _ready():
 func _physics_process(delta):
 	get_move_input(delta)
 	velocity = move_and_slide(velocity)
-	check_for_carve()
-	previous_y_velocity = velocity.y
-
+	
 func get_move_input(delta):
 	var input_y = 0.0
 	if Input.is_action_pressed("move_down"):
 		input_y += 1
 	if Input.is_action_pressed("move_up"):
 		input_y -= 1
+	check_for_carve(input_y)
 	var target_velocity_y = input_y * max_speed
 	if input_y != 0:
 		velocity.y = move_toward(velocity.y, target_velocity_y, acceleration * delta)
 	else:
 		velocity.y = move_toward(velocity.y, 0, drag_friction * delta)
 	velocity.x = scroll_speed
-func check_for_carve():
-	var y_sign_changed = sign(velocity.y) != sign(previous_y_velocity)
-	var was_moving_fast_enough = abs(previous_y_velocity) > carve_threshold
+func check_for_carve(input_y):
+	# 카빙 조건 정의
+	var input_is_opposite = input_y != 0 and sign(input_y) != sign(velocity.y) and sign(velocity.y) != 0
+	var is_moving_fast_enough = abs(velocity.y) > carve_threshold
 	
-	if y_sign_changed and was_moving_fast_enough:
+	# [수정] 카빙 조건을 만족 '그리고' 현재 카빙 상태가 아닐 때
+	if input_is_opposite and is_moving_fast_enough and not is_carving:
+		# '사건' 발생!
 		spawn_splash_attack()
-		
+		# '상태' 변경: 카빙이 시작됐으니 스위치를 내립니다.
+		is_carving = true
+	
+	# [수정] 카빙 조건이 깨졌을 때 (예: 턴이 완료되거나 키를 뗐을 때)
+	elif not (input_is_opposite and is_moving_fast_enough):
+		# '상태' 리셋: 스위치를 다시 올려서 다음 카빙을 준비합니다.
+		is_carving = false	
 func spawn_splash_attack():
 	print("Yeah!")
 	
--- Scenes/World.gd
+++ Scenes/World.gd
@@ -4,12 +4,13 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+export (int) var scroll_speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
+func scroll_background():
+	$Ocean.position.x -= scroll_speed 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
